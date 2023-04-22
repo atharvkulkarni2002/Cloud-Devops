c2e6e13578e337c0e7f4e5e4bb2cbb5f0eeb04d6
Atharv-2002-Kulkarni <atahrvkulkarni378@gmail.com>
2023-04-22 18:41:59 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The CI/CD pipeline has been created
         </p>
         <a
           className="App-link"
