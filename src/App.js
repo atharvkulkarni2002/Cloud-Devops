2608d4c504274efa4020d0ca2d8496334c026173
Amey <ameyswami45@gmail.com>
2023-04-26 01:32:01 +0530

    Amit's Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import PropTypes from "prop-types";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
+  const [Mode, setMode] = useState("light");
+  const [alert, setalert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  };
+  const toggleMode = () => {
+    if (Mode === "light") {
+      setMode("dark");
+
+      document.body.style.backgroundColor = "#212529";
+
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The CI/CD pipeline has been created
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Navbar title="TextUtil" Mode={Mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route exact path="/about" element={<About/>}/>
+          
+          
+          <Route exact path="/" element={ <TextForm
+          heading="Enter your text to analyze "
+          Mode={Mode}
+          showAlert={showAlert}
+        />}/>
+         
+          
+        </Routes>
+       
+
+      
+      </div>
+      </Router>
+
+    </>
   );
 }
 
